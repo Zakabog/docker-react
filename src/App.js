589a9fdedc7ac4df952176b483df946f1fd9d1ad
banshee <banshee@FUPA.slavelabor>
2020-03-08 18:48:53 -0400

    Changed app text for feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New Text!
+          This is the new feature branch!
         </p>
         <a
           className="App-link"
